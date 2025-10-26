b5fd8f23330506e14ee331fadb3558f84231024e
Haxxolotto <62444678+Haxxxxxx@users.noreply.github.com>
2025-10-27 00:47:19 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ module.exports = {
   expo: {
     name: "WeSplit",
     slug: "WeSplit",
-    version: "1.0.2",
+    version: "1.0.3",
     orientation: "portrait",
     icon: "./assets/android-app-icon-no-alpha.png",
     userInterfaceStyle: "light",
