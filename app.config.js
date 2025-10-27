347d8c91933f41c755bec848fd5a18fb37244e02
Haxxolotto <62444678+Haxxxxxx@users.noreply.github.com>
2025-10-27 17:40:31 +0100

    Update app.config.js

--- app.config.js
+++ app.config.js
@@ -15,7 +15,7 @@ module.exports = {
       supportsTablet: true,
       bundleIdentifier: "com.wesplit.app",
       displayName: "WeSplit Beta",
-      buildNumber: "6",
+      buildNumber: "7",
       deploymentTarget: "15.1",
       infoPlist: {
         LSApplicationQueriesSchemes: [
@@ -41,7 +41,7 @@ module.exports = {
     android: {
       package: "com.wesplit.app",
       displayName: "WeSplit Beta",
-      versionCode: 10006,
+      versionCode: 10007,
       adaptiveIcon: {
         foregroundImage: "./assets/android-app-icon-no-alpha.png",
         backgroundColor: "#061113"
